1f67ee1b2a0e298c61c07f7d94273e345b4889e1
Rizki Joelisman <email": null>
2017-10-25 15:57:08 +0700

    baru input model

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const mongoose = require("mongoose")
 mongoose.connect('mongodb://rizqjoe:admin123@cluster0-shard-00-00-l8k4w.mongodb.net:27017,cluster0-shard-00-01-l8k4w.mongodb.net:27017,cluster0-shard-00-02-l8k4w.mongodb.net:27017/kopi?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin')
 
 
+
+// const Kopi = require("./routers/kopi")
+
+
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/*+json' }))
@@ -13,6 +17,9 @@ app.use(bodyParser.json({type: 'application/x-www-form-urlencoded'}))
 
 app.use(cors())
 
+// app.get('/', (req,res)=>{
+//     res.json('mongoose connected')
+// })
 
 
 app,listen(3000,()=>{
---
+++ server/model/Kopi.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const kopiSchema = mongoose.Schema({
+    
+    
+    title: {
+        type: String,
+        require: true
+    },
+
+    category:{
+        type: String,
+        require: true
+    },
+    
+    stock: {
+        type: Number,
+        require: true
+    },
+
+    img: {
+        type: String,
+        require: True
+    },
+
+    price: {
+        type: Number,
+        require: true
+    }
+
+})
+
+const Kopi = mongoose.model('Kopi', kopiSchema)
+
+module.exports = Kopi
