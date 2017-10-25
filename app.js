a5feb4cd50595175ddb8c17b0fd31742af2235d8
Rizki Joelisman <email": null>
2017-10-25 15:32:41 +0700

    using mongo atlas

--- app.js
+++ app.js
@@ -3,4 +3,18 @@ const app = express();
 const bodyParser = require('body-parser')
 const cors = require("cors")
 const mongoose = require("mongoose")
-mongoose.connect('')
+mongoose.connect('mongodb://rizqjoe:admin123@cluster0-shard-00-00-l8k4w.mongodb.net:27017,cluster0-shard-00-01-l8k4w.mongodb.net:27017,cluster0-shard-00-02-l8k4w.mongodb.net:27017/kopi?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin')
+
+
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/*+json' }))
+app.use(bodyParser.json({type: 'application/x-www-form-urlencoded'}))
+
+app.use(cors())
+
+
+
+app,listen(3000,()=>{
+    console.log("listen port 3000")
+})
